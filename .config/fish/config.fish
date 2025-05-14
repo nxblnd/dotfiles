e8d0ce64d3791ccb255b54e005190c17ba9cf99f
Pavel Egorov <contact@pavelegorov.xyz>
2025-05-15 00:33:50 +0300

    [fish] add ls abbreviation to eza

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 pyenv init - | source
 if status is-interactive
     abbr --add dotfiles git --git-dir="$HOME/.config/dotfiles/" --work-tree="$HOME"
+    abbr --add ls eza -lAb --git --git-repos
 
     set -gx MANPAGER 'nvim +Man!'
 end
