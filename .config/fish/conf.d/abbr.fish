f4d454263177f74c6fd3eb8424e927516646ba8c
Pavel Egorov <contact@pavelegorov.xyz>
2025-06-12 16:18:22 +0300

    [fish] change ls from abbr to alias

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -6,9 +6,6 @@ abbr --add gs "git status"
 abbr --add ga "git add"
 abbr --add gco "git checkout"
 
-# ls & alternatives
-abbr --add ls "eza -lAb --git --git-repos"
-
 # Added proxy flags
 set SSPROXY 'socks5://localhost:1080'
 abbr --add curlss "curl --proxy=$SSPROXY"
---
+++ .config/fish/conf.d/alias.fish
@@ -0,0 +1,3 @@
+# ls & alternatives
+alias ls "eza --long --almost-all --binary --smart-group --group-directories-first --git --git-repos --color=auto --icons=never"
+alias lss "ls --total-size"
