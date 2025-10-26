e8b9b44f7823db988af04b157ac6a0b4599282a9
Pavel Egorov <contact@pavelegorov.xyz>
2025-10-26 09:01:13 +0300

    [fish] add comment to file

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -2,5 +2,7 @@ set -x EDITOR 'nvim'
 set -x VISUAL 'nvim'
 set -x MANPAGER 'nvim +Man!'
 set -x BROWSER 'firefox'
-set -x SUDO_PROMPT '[sudo] password for %p: '
+set -x SUDO_PROMPT '[sudo] password for %p: ' # Adds bell to sudo prompt
+set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+
 
