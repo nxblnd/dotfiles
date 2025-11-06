6318cbe28c5bdad7f05cb29dd329dfc94ca8c3e7
Pavel Egorov <contact@pavelegorov.xyz>
2025-11-07 02:25:46 +0300

    [fish] change ls aliases to regular function definitions

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -1,5 +1,11 @@
 # ls & alternatives
 if type -q eza
-    alias ls "eza --long --almost-all --binary --smart-group --group-directories-first --git --git-repos --color=auto --icons=never"
-    alias lss "ls --total-size"
+    function ls --wraps eza
+        eza --long --almost-all --binary --smart-group --group-directories-first --git --git-repos --color=auto --icons=never
+    end
+
+    function lss --wraps eza --description "ls with total dir size"
+        ls --total-size
+    end
 end
+
