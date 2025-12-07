c45e377cda2ee0990e5833b425332c0832e7f2f1
Pavel Egorov <contact@pavelegorov.xyz>
2025-12-07 23:33:08 +0300

    [fish] fix fisher auto installation

--- .config/fish/conf.d/00-fisher-install.fish
+++ .config/fish/conf.d/00-fisher-install.fish
@@ -1,4 +1,6 @@
-if not type -q fisher
+if not type -q fisher && not test "$FISHER_INSTALL_STARTED"
+    set -U FISHER_INSTALL_STARTED true
     curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
     fisher update
+    set -e FISHER_INSTALL_STARTED
 end
