ba1375a9fc4d9f9fb3ffb57c1231f06eebfa4f28
Pavel Egorov <contact@pavelegorov.xyz>
2025-12-08 23:10:33 +0300

    [fish] Add additional env variables

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -4,5 +4,3 @@ set -x MANPAGER 'nvim +Man!'
 set -x BROWSER 'firefox'
 set -x SUDO_PROMPT '[sudo] password for %p: ' # Adds bell to sudo prompt
 set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
-
-
