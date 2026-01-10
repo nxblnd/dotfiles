7444ed1b1d64825aa4e7eeed12c97e8bb9dfdef2
Pavel Egorov <contact@pavelegorov.xyz>
2026-01-10 04:06:01 +0300

    [fish] set LOCAL_SCRIPTS env variable

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -4,4 +4,6 @@ set -x MANPAGER 'nvim +Man!'
 set -x BROWSER 'firefox'
 set -x SUDO_PROMPT '[sudo] password for %p: ' # Adds bell to sudo prompt
 set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+
 set -x LOCAL_BIN "$HOME/.local/bin"
+set -x LOCAL_SCRIPTS "$HOME/.local/scripts"
