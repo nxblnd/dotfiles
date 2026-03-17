998b20850ef8e0017d08c007d2715a382fb8e206
Pavel Egorov <contact@pavelegorov.xyz>
2026-03-17 19:10:58 +0300

    [fish] move python-related envs into python config

--- .config/fish/conf.d/python.fish
+++ .config/fish/conf.d/python.fish
@@ -3,3 +3,7 @@ if type -q pyenv && status is-interactive
 end
 
 set -x PYTHONSTARTUP "$XDG_CONFIG_HOME/pythonrc.py"
+set -x PYTHONHISTORY "$XDG_STATE_HOME/python_history"
+set -x IPYTHONDIR "$XDG_CONFIG_HOME/ipython"
+set -x JUPITER_CONFIG_DIR "$XDG_CONFIG_HOME/jupyter"
+set -x PYENV_ROOT "$XDG_DATA_HOME/pyenv"
